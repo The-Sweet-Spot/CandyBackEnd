ceb9ca488b09425c44f0ac544f2bab5a9db15f5d
Tori Deblinger <vdeblinger@yahoo.com>
2022-12-05 17:21:55 -0600

    add department id

--- db/SweetProducts.js
+++ db/SweetProducts.js
@@ -2,16 +2,16 @@ const { client } = require('./index')
 
 
 
-async function createSweetProduct({sweetsName, description, stock, price, departmentId}) {
+async function createSweetProduct({sweetsName, description, stock, price, image, departmentId}) {
     try {
         console.log()
         
         const { rows: [sweet_products] } = await client.query(`
-        INSERT INTO sweet_products("sweetsName", description, stock, price, "departmentId")
-        VALUES($1, $2, $3, $4, $5)
+        INSERT INTO sweet_products("sweetsName", description, stock, price, image, "departmentId")
+        VALUES($1, $2, $3, $4, $5, $6)
         ON CONFLICT ("sweetsName") DO NOTHING
         RETURNING *;
-        `, [sweetsName, description, stock, price, departmentId]);
+        `, [sweetsName, description, stock, price, image, departmentId]);
         return sweet_products
     } catch (error) {
         console.log(error)
